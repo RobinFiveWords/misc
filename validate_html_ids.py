74be76358dbb5e369781bd6bc55fc1940ee40f8e
Robin Fishbein <RobinFiveWords@users.noreply.github.com>
2024-02-16 22:34:57 -0600

    added Latin and ANSI encodings and corrected valid id regex

--- validate_html_ids.py
+++ validate_html_ids.py
@@ -4,15 +4,28 @@ import glob
 import itertools
 import re
 
-parser = argparse.ArgumentParser()
+description = ' '.join(['report values for id attributes',
+                        'that appear more than once,',
+                        'or are not valid according to the HTML specification',
+                        '(are blank or contain ASCII whitespace)'])
+parser = argparse.ArgumentParser(description=description)
 parser.add_argument('FILE', nargs='+',
                     help='one or more filenames or globs')
 parser.add_argument('-r', '--recursive', action='store_true',
                     help='search in any subfolders as well')
+encodings = parser.add_mutually_exclusive_group()
+encodings.add_argument('-a', action='store_true',
+                       help='encoding: ANSI (Windows only)')
+encodings.add_argument('-l', action='store_true',
+                       help='encoding: Latin1 / ISO-8859')
+encodings.add_argument('-u', action='store_true',
+                       help='encoding: UTF-8 (default)')
+
 args = parser.parse_args()
 
 regex_id_attribute = re.compile(r'id="([^"]+)"')
-regex_valid_html_id = re.compile(r'^[A-Za-z][A-Za-z0-9_:\-\.]*$')
+# https://html.spec.whatwg.org/multipage/dom.html#the-id-attribute
+regex_valid_html_id = re.compile(r'^[^\s]+$')
 
 if args.recursive:
   prefix = f"**{'' if args.FILE[0] == '/' else '/'}"
@@ -23,14 +36,26 @@ else:
   files = itertools.chain.from_iterable(
             glob.glob(GLOB) for GLOB in args.FILE)
 
+if args.l:
+  encoding = 'latin'
+elif args.a:
+  encoding = 'ansi'
+else:
+  encoding = 'utf-8'
+
 seen = set()
 for file in files:
   if file in seen:
     continue
   seen.add(file)
   anomalies = []
-  with open(file) as f:
-    page = f.read()
+  with open(file, encoding=encoding) as f:
+    try:
+      page = f.read()
+    except UnicodeDecodeError as e:
+      print(file)
+      print(f"  not in {encoding} encoding")
+      raise e
   id_counts = collections.Counter(regex_id_attribute.findall(page))
   for id_value, count in id_counts.items():
     if not regex_valid_html_id.match(id_value):
